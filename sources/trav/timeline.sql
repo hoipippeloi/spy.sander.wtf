4f148adf3476c7fe22da8ec4d9bdc45c9c490eaf
Patrick Tehubijuluw <creatuluw@gmail.com>
2025-02-11 15:03:47 +0100

    add moves

--- sources/trav/timeline.sql
+++ sources/trav/timeline.sql
@@ -19,5 +19,25 @@ FROM
     JOIN s1_vdata v ON bl.wid = v.wref
     JOIN s1_users u ON v.owner = u.id
 
+UNION
+
+SELECT
+    'Movement Activity' as activity_type,
+    CONCAT(u1.username, ' → ', u2.username) as username,
+    DATE_FORMAT(FROM_UNIXTIME(m.starttime), '%d-%m-%Y %H:%i:%s') as formatted_time,
+    CASE 
+        WHEN m.sort_type = 3 THEN 'Attack'
+        WHEN m.sort_type = 4 THEN 'Raid'
+        WHEN m.sort_type = 2 THEN 'Return'
+        WHEN m.sort_type = 1 THEN 'Reinforcement'
+        ELSE 'Unknown movement'
+    END as description
+FROM 
+    s1_movement m
+    JOIN s1_vdata v1 ON m.from = v1.wref
+    JOIN s1_vdata v2 ON m.to = v2.wref
+    JOIN s1_users u1 ON v1.owner = u1.id
+    JOIN s1_users u2 ON v2.owner = u2.id
+
 ORDER BY 
     STR_TO_DATE(formatted_time, '%d-%m-%Y %H:%i:%s') DESC
